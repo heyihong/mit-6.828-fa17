ac11ac84e969fa6fe96d81ef6f6780b76696621f
Yihong He <heyihong.cn@gmail.com>
2018-02-05 18:50:43 -0800

    finish hw11

--- xv6-public/log.c
+++ xv6-public/log.c
@@ -72,11 +72,8 @@ install_trans(void)
   int tail;
 
   for (tail = 0; tail < log.lh.n; tail++) {
-    struct buf *lbuf = bread(log.dev, log.start+tail+1); // read log block
     struct buf *dbuf = bread(log.dev, log.lh.block[tail]); // read dst
-    memmove(dbuf->data, lbuf->data, BSIZE);  // copy block to dst
     bwrite(dbuf);  // write dst to disk
-    brelse(lbuf);
     brelse(dbuf);
   }
 }
